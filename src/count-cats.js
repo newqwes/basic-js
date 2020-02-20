0927669f9af65772eb66241ba1ff643bbbe8b635
Nazar Ashirov <newnewqwes@gmail.com>
2020-02-20 16:48:50 +0300

    init: Done! (Count cats)

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(catMatrix) {
+
+  let backyardCats = catMatrix.flat(); //flat убираем вложенность массивов. На сколько уровней вложенности уменьшается мерность исходного массива. По-умолчанию 1.
+
+  let caught = backyardCats.filter(function(currentValue) { //filter ищит в массиве значение и добавляет это в новый массив caught
+    return currentValue == "^^";
+  });
+  
+  return caught.length;
+  
 };
